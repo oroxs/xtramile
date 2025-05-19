fa7b6c9a2835907f5bc372ba6d2210fe3ba4539b
Vedy Aditya Nugraha <aku.vedy@gmail.com>
2025-05-19 13:31:05 +0700

    Update login using before each

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,55 +3,41 @@ const { LoginPage } = require('../pages/LoginPage');
 const { InventoryPage } = require('../pages/InventoryPage');
 const user = require ('../data/data-user');
 
-test('Successful login with standard user', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+let loginPage;
+let inventoryPage;
 
+test.beforeEach(async ({ page }) => {
+  loginPage = new LoginPage(page);
+  inventoryPage = new InventoryPage(page);
   await loginPage.goto();
+});
 
-  await loginPage.login(user.standardUser.username,user.standardUser.password);
-
+test('Successful login with standard user', async ({ page }) => {
+  await loginPage.login(user.standardUser.username, user.standardUser.password);
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
-test('Login fails with invalid credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-
-  await loginPage.login(user.invalidUser.username,user.invalidUser.password);
-
+test('Login fails with invalid credentials', async () => {
+  await loginPage.login(user.invalidUser.username, user.invalidUser.password);
   await expect(loginPage.errorMessage).toBeVisible();
   await expect(loginPage.errorMessage).toContainText('Username and password do not match');
 });
 
-test('Login fails with empty username and password', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-
+test('Login fails with empty username and password', async () => {
   await loginPage.login('', '');
-
   await expect(loginPage.errorMessage).toBeVisible();
   await expect(loginPage.errorMessage).toContainText('Username is required');
 });
 
-test('Logout from the app and verify redirect to login page', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const inventoryPage = new InventoryPage(page);
-  
-    await loginPage.goto();
-    await loginPage.login(user.standardUser.username,user.standardUser.password);
-  
-    await inventoryPage.logout();
-  
-    await expect(page).toHaveURL('https://www.saucedemo.com/');
-    await expect(page.locator('#login-button')).toBeVisible();
-  });
-
-test('Login fails with locked out user', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-
-  await loginPage.login(user.lockedOutUser.username,user.lockedOutUser.password);
-
+test('Login fails with locked out user', async () => {
+  await loginPage.login(user.lockedOutUser.username, user.lockedOutUser.password);
   await expect(loginPage.errorMessage).toBeVisible();
   await expect(loginPage.errorMessage).toContainText('Sorry, this user has been locked out.');
+});
+
+test('Logout from the app and verify redirect to login page', async ({ page }) => {
+  await loginPage.login(user.standardUser.username, user.standardUser.password);
+  await inventoryPage.logout();
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+  await expect(page.locator('#login-button')).toBeVisible();
 });
